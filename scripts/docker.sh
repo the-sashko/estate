8adff44dd4c99d9c67a2cf777535d7024bed7809
The Sashko <sashko.me>
2021-02-04 00:29:16 +0200

    move Dockerfile

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,6 +3,8 @@
 currDir=$(pwd)
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
+cd "$scriptDir/.."
+
 cp "$scriptDir/cron.sh" cron.sh
 
 rm -rf .git
@@ -15,14 +17,16 @@ rm -f .gitignore
 rm -f LICENSE
 rm -f requirements.txt
 
-cd "$scriptDir/../docker"
+cd docker
 
 sudo docker build -t estate_parser .
 
-cd "$currDir"
+cd ..
 
 mkdir data
 
 sudo docker run -d -p 80:80 --name estate_parser -v "$(pwd)/data":/storage/estate_parser/data estate_parser
+
+cd "$currDir"
 
 exit
