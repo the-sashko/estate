f814321cfbc5c2c8db7db8db7d5bb7c4a19e7afc
The Sashko <sashko.me>
2021-02-04 00:11:02 +0200

    improve docker build script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,6 +3,18 @@
 currDir=$(pwd)
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
+cp "$scriptDir/cron.sh" cron.sh
+
+rm -rf .git
+rm -rf data
+rm -rf install
+rm -rf scripts
+rm -rf src
+
+rm -f .gitignore
+rm -f LICENSE
+rm -f requirements.txt
+
 cd "$scriptDir/../docker"
 
 sudo docker build -t estate_parser .
@@ -12,7 +24,5 @@ cd "$currDir"
 mkdir data
 
 sudo docker run -d -p 80:80 --name estate_parser -v "$(pwd)/data":/storage/estate_parser/data estate_parser
-
-cp "$scriptDir/cron.sh" cron.sh
 
 exit
