9bae6e40b3f42f8667b60b4869858a15e4f8a8c0
The Sashko <18634500+the-sashko@users.noreply.github.com>
2021-02-06 02:26:27 +0200

    improve docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,6 +7,14 @@ cd "$scriptDir/.." || exit 1
 
 cp "$scriptDir/cron.sh" cron.sh
 
+sudo docker build -t estate_parser .
+
+sudo docker run -d --name estate_parser -v "$(pwd)/data":/storage/estate_parser/data estate_parser            
+
+rm -rf data
+
+./scripts/install.sh
+
 rm -rf .git
 rm -rf install
 rm -rf scripts
@@ -14,17 +22,17 @@ rm -rf docs
 rm -rf src
 rm -rf config
 rm -rf bin
+rm -rf include
+rm -rf lib
+rm -rf lib64
 
 rm -f .gitignore
 rm -f LICENSE
 rm -f README.md
 rm -f requirements.txt
-
-sudo docker build -t estate_parser .
-
-rm -rf Dockerfile
-
-sudo docker run -it --name estate_parser -v "$(pwd)/data":/storage/estate_parser/data estate_parser vi /storage/estate_parser/config/telegram.json
+rm -f Dockerfile
+rm -f pip-selfcheck.json
+rm -f pyvenv.cfg
 
 cd "$currDir" || exit 1
 
